2c5266ec8ff88306a395761138da33959a3ee4dd
Isha Kumari <149878382+isha31127@users.noreply.github.com>
2026-08-21 14:45:00 +0530

    Time: 28 ms (18.42%) | Memory: 91.2 MB (48.33%) - LeetSync

--- 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -3,19 +3,16 @@ public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         int n = rocks.size();
         int c = 0;
-        unordered_map<int,int>mp;
-        for(int i = 0; i < n; i++){
-            int d = capacity[i] - rocks[i];
-            mp[i] = d;
+        vector<int> diff(n);
+        for(int i = 0;i < n; i++){
+            diff[i] = capacity[i] - rocks[i];
+            
         }
-        vector<pair<int,int>> vec1(mp.begin(),mp.end());
-        sort(vec1.begin(),vec1.end(),[](const auto& a,const auto& b){
-            return a.second < b.second;
-        });
-        for(const auto& [key,value] : vec1){
-            if(value <= additionalRocks){
-            additionalRocks -= value;
-            c++;
+        sort(diff.begin(),diff.end());
+        for(int i = 0; i < n; i++){
+            if(diff[i] <= additionalRocks){
+                additionalRocks -= diff[i];
+                c++;
             }
         }
         return c;
